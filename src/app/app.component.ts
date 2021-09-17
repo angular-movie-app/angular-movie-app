d9b62ece29777a92c83b71b9e237e4bdc99b6572
Kade <KadeRZ0021@gmail.com>
2021-09-17 08:24:52 -0600

    merge with content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,9 +43,9 @@ export class AppComponent {
     })
   }
   errorText: string = ""
-  constructor(private apiService: GetAPIService) { }
-  title = 'angular-movie-app'; 
-  posts: any;
+  // constructor(private apiService: GetAPIService) { }
+  // title = 'angular-movie-app'; 
+  // posts: any;
   // https://medium.com/swlh/how-to-get-an-api-with-angular-10-948f2d10dc5
   ngOnInit() {
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  api: "api.themoviedb.org/3/search/movie?api_key=3b05444824eda7017c57640089b4e650"
+  api: "api.themoviedb.org/3/search/movie?api_key=3b05444824eda7017c57640089b4e650",
+  firebase: {
+    apiKey: "AIzaSyAsr52o-xTLC9OGngolD8N1VWIwqP3Hp7E",
+    authDomain: "angular-movie-app-38950.firebaseapp.com",
+    projectId: "angular-movie-app-38950",
+    storageBucket: "angular-movie-app-38950.appspot.com",
+    messagingSenderId: "945935627379",
+    appId: "1:945935627379:web:f2f3aabde0a0fda56be606",
+    measurementId: "G-4NXMQ8N34Y"
+  }
 };
 
 /*
