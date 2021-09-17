93cc45005ad56d5934ce636c4ebb352fa4b1d7ea
Kade Zimmerman <kaderz0021@gmail.com>
2021-09-17 08:03:14 -0600

    content merge

---
+++ src/app/movie.ts
@@ -0,0 +1,9 @@
+export interface MovieItem {
+    title: string
+    tagline: string
+    id: number
+    voteAverage: number
+    voteCount: number
+    runtime?: number
+    posterPath?: string
+  }
